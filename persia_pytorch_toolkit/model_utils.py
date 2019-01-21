ccce38ff5010861ccf1a6437a87f1b7d82faf349
Xiangru Lian <admin@mail.xrlian.com>
2019-01-20 20:38:29 -0500

    add model flatten function

--- persia_pytorch_toolkit/model_utils.py
+++ persia_pytorch_toolkit/model_utils.py
@@ -27,3 +27,48 @@ def get_model_size(model):
             tmp *= x
         params += tmp
     return params
+
+
+def flatten_model_and_grad_tensor(model, verbose=False):
+    """
+    caveats:
+    1. can only be called after first loss.backward (so that grad variables are created)
+    2. the tensors are all zero (so if you want to initialization plz reinitialize)
+    """
+    total_size = 0
+    tensor_type = None
+    for parameter in network.parameters():
+        if tensor_type == None:
+            tensor_type = parameter.data.type() # https://github.com/pytorch/pytorch/wiki/Breaking-Changes-from-Variable-and-Tensor-merge
+        total_size += parameter.nelement()
+
+    if verbose:
+        print("total size: ", total_size)
+        print("tensor type: ", tensor_type)
+
+    tensor = torch.Tensor(total_size).type(tensor_type)
+    storage = tensor.storage()
+    grad_tensor = torch.Tensor(total_size).type(tensor_type)
+    grad_storage = grad_tensor.storage()
+
+
+    if verbose:
+        print("create new continuous storage")
+
+    current_offset = 0
+
+    for parameter in network.parameters():
+        backup = parameter.data.clone()
+        parameter.data.set_(storage, current_offset, parameter.data.size())
+        parameter.data.copy_(backup)
+        parameter.grad.data.set_(grad_storage, current_offset, parameter.data.size())
+        current_offset += parameter.data.nelement()
+        print("parameter storage offset: ", parameter.data.storage_offset())
+
+    if verbose:
+        print("The C pointer: ", storage.data_ptr())
+        print("The C pointer for grad: ", grad_storage.data_ptr())
+        print("Is contiguous memory? ", tensor.is_contiguous())
+        print("Is grad contiguous memory? ", grad_tensor.is_contiguous())
+
+    return tensor, grad_tensor
